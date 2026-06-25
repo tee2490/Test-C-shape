773e49aacc80936a4f08b8b8bf4a5ff7bea89917
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-06-25 09:58:28 +0700

    Add project files.

---
+++ pro1/Program.cs
@@ -0,0 +1,21 @@
+﻿//การแสดงผล
+Console.WriteLine("Hello Cs69");
+Console.WriteLine(100);
+Console.WriteLine(1 + 2);
+Console.WriteLine();
+
+//ตัวแปร
+int num1, num2, ans;  //memory
+string name;
+
+num1 = 1;
+num2 = 2;
+ans = num1 + num2;
+
+name = "Hello you";
+
+Console.WriteLine($"num1 = {num1}");
+Console.WriteLine($"num2 = {num2}");
+Console.WriteLine($"ans = {ans}");
+
+Console.WriteLine($"name = {name}");
